27ff7dc7025f7affdaab6e0e327623efeb797bbb
onkyoh <adnanradwan_8@hotmail.com>
2026-05-20 18:21:33 -0400

    first page done

--- src/assets/js/carousel.js
+++ src/assets/js/carousel.js
@@ -1,16 +1,21 @@
 import EmblaCarousel from 'embla-carousel';
+import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures';
 
 const viewport = document.querySelector('#carousel .cs-carousel-viewport');
 
 if (viewport) {
     const progressBar = document.querySelector('#carousel .cs-carousel-bar');
 
-    const embla = EmblaCarousel(viewport, {
-        loop: false,
-        align: 'start',
-        containScroll: 'trimSnaps',
-        dragFree: false,
-    });
+    const embla = EmblaCarousel(
+        viewport,
+        {
+            loop: false,
+            align: 'start',
+            containScroll: 'trimSnaps',
+            dragFree: false,
+        },
+        [WheelGesturesPlugin()],
+    );
 
     const updateProgress = () => {
         if (!progressBar) return;
